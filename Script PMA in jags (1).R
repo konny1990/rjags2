6b9a316057d440ca41e474b6b770807863307235
konny1990 <konny1990@users.noreply.github.com>
2019-02-13 21:17:51 +0100

    Update Script PMA in jags (1).R
    etoimo0

--- Script PMA in jags (1).R
+++ Script PMA in jags (1).R
@@ -1,13 +1,17 @@
 install.packages("R2jags")
+install.packages("jags")
 library(R2jags)
+library(jags)
 help(jags)
 
 
 #######################
 #this is the data
 #######################
-mydata = list(ns=3,m=cbind(c(187.5,193.4,161.3),c(165.6,171.9,180.5)),sd=cbind(c(12.3, 8.5,6.9),c(11.9, 10.8, 11.1)),n=cbind(c(12,13,14),c(14,15,16)))
-mydata
+
+#to prec to evala afou to ipologise i R, giati den to angnorize meta to jag
+mydata = list(ns=3,prec=c(4.59,4.36,4.91),m=cbind(c(187.5,193.4,161.3),c(165.6,171.9,180.5)),sd=cbind(c(12.3, 8.5,6.9),c(11.9, 10.8, 11.1)),n=cbind(c(12,13,14),c(14,15,16)))
+#mydata
 MD=c()
 mydata$ns
 #Ypologismos tou SMD
@@ -16,6 +20,7 @@ Swithinpar= c()
 Swithin=c()
 SMD=c()
 Var=c()
+prec=c()
 for (i in 1:mydata$ns) {
   #MD=M2-M1
   MD[i]=mydata$m[i,2]-mydata$m[i,1]
@@ -24,12 +29,12 @@ for (i in 1:mydata$ns) {
   #paranomastis tou Swithin
   Swithinpar[i]=(mydata$n[i,1])+(mydata$n[i,2])-2
   #Swithin
-  Swithin[i]=sqrt( Swithinar[i]/Swithinpar[i])
+  Swithin[i]=sqrt(Swithinar[i]/Swithinpar[i])
   #SMD
   SMD[i]=MD[i]/Swithin[i]
   #Var tou Smd
   Var[i]=((mydata$n[i,1]+mydata$n[i,2])/(mydata$n[i,1]*mydata$n[i,2]))+(SMD[i]^2)/(2*(mydata$n[i,1]+mydata$n[i,2]))
-
+  prec[i]=1/Var[i]
 }
 MD
 Swithinar
@@ -37,33 +42,29 @@ Swithinpar
 Swithin
 SMD
 Var
+prec
 
 
 #######################
 #then make the model
-#######################
+#
+
+########################
 PMAcontinuous=function() {
   
   for(i in 1:ns) { 
-    
     #likelihood
+    
     SMD[i]~ dnorm(theta[i],prec[i])
     theta[i]~dnorm(mean,prec.tau)
     
-    #parametrisation          
-    logit(p[i,1])<- u[i]
-    logit(p[i,2])<- u[i] + theta[i] # OR should I use logit(p[i,2])<-lpi2 and  lpi2 ~ sum(u[i] + theta[i])
-    theta[i] ~ dnorm(mean,prec)
     }
   
     #prior distributions
-      for (i in 1:ns) {u[i] ~ dnorm(0,.01)}
       tau ~ dunif(0,1)   #dnorm(0,100)%_%T(0,)                                 
-      prec<- 1/pow(tau,2)
-      mean ~ dnorm(0,100)
-}#end of model
-
-#######################
+      prec.tau<- 1/pow(tau,2)
+      mean ~ dnorm(0,0.01)
+}#end of model#####################
 # initial values
 #######################
 
@@ -73,20 +74,11 @@ initialval = NULL
 #######################
 # run the model
 #######################
+PMAinJAGS<- jags(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 2, n.iter = 10000, n.burnin = 1000, DIC=F, model.file = PMAcontinuous)
 
-PMAinJAGS<- jags(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 2, n.iter = 10000, n.burnin = 1000, DIC=F, model.file = PMAbinary)
 
 #results
 print(PMAinJAGS)
-
-#check chain mixing
-traceplot(PMAinJAGS)
-
-#what else is there
-names(PMAinJAGS)
-names(PMAinJAGS$BUGSoutput)
-PMAinJAGS$BUGSoutput$summary
-
 
 #///////////////////////////
 
@@ -95,12 +87,12 @@ PMAinJAGS$BUGSoutput$summary
 #######################
 
 start_time <- Sys.time()
-PMAinJAGS<- jags(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 4, n.iter = 100000, n.burnin = 1000, DIC=F, model.file = PMAbinary)
+PMAinJAGS<- jags(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 4, n.iter = 100000, n.burnin = 1000, DIC=F, model.file = PMAcontinuous)
 end_time <- Sys.time()
 end_time - start_time
 
 start_time <- Sys.time()
-PMAinJAGS<- jags.parallel(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 4, n.iter = 100000, n.burnin = 1000, DIC=F, model.file = PMAbinary)
+PMAinJAGS<- jags.parallel(mydata,initialval,parameters.to.save = c("tau","mean"), n.chains = 4, n.iter = 100000, n.burnin = 1000, DIC=F, model.file = PMAcontinuous)
 end_time <- Sys.time()
 end_time - start_time
 
